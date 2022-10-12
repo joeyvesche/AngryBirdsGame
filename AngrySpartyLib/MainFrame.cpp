7d62766836d70c7c54eeb4361587a30219542538
skaggswi <skaggswi@msu.edu>
2022-10-11 21:47:16 -0400

    Added menu bar and menus

--- AngrySpartyLib/MainFrame.cpp
+++ AngrySpartyLib/MainFrame.cpp
@@ -10,4 +10,26 @@ void MainFrame::Initialize()
 {
     Create(nullptr, wxID_ANY, L"Angry Sparty",
             wxDefaultPosition, wxSize(1600, 900));
+
+    // Create a menu bar
+    auto menuBar = new wxMenuBar();
+
+    // File menu
+    auto fileMenu = new wxMenu();
+    menuBar->Append(fileMenu, L"&File");
+
+    // Level menu
+    auto levelMenu = new wxMenu();
+    menuBar->Append(levelMenu, L"&Level");
+
+    // View menu
+    auto viewMenu = new wxMenu();
+    menuBar->Append(viewMenu, L"&View");
+
+    // Help menu
+    auto helpMenu = new wxMenu();
+    menuBar->Append(helpMenu, L"&Help");
+
+    // Set the menu bar to this frame
+    SetMenuBar(menuBar);
 }
